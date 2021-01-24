05696c4129ba4cb515c4f23c125e7ff9642db7fd
Sonsuz Dongu <siberpoyraz66@yandex.com>
2021-01-24 19:58:19 +0300

    Add project files.

---
+++ Collection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Engin","Murat","Poyraz","Kerem","Halil" };
+            isimler.Add("kerem");
+
+            Console.WriteLine(isimler[5]);
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using OOP1;
+using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsStock = 3;
+
+
+            Product product2 = new Product 
+            { Id = 5, CategoryId = 2, 
+                ProductName = "Bilgisayar", UnitsStock = 5, UnitPrice = 500 };
+
+            ProductManagercs productManager = new ProductManagercs();
+            productManager.Add(product2);
+
+
+
+
+        }
+    }
+}
